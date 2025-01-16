ff48623d32849ef6865de426083db44714a24770
Vikas <vikas.kumar.singh1198@gmail.com>
2025-01-16 19:30:57 +0530

    Created Custom hook to fetch data from Supabase

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,4 +1,19 @@
+import { getJobs } from "@/api/apiJobs"
+import useFetch from "@/hooks/use-fetch"
+import { useEffect } from "react";
 const Joblisting = () => {
+
+  const {fn:fnJobs
+    ,data:dataJobs,
+    loading:loadingJobs
+  } = useFetch(getJobs);
+  
+  console.log(dataJobs);
+
+  useEffect(()=>{
+    fnJobs()
+  },[])
+
   return (
     <div>Joblisting</div>
   )
